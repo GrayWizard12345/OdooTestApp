d2d558b00084dcde5356064854edb623aa3fa97a
Muslimbek Abduganiev <muslim4ik14@gmail.com>
2024-03-29 16:37:11 +0500

    Initializing project

---
+++ src/education_center/models/course_group.py
@@ -0,0 +1,7 @@
+"""
+Copyright (c) 2024, Muslimbek Abduganiev
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+"""
